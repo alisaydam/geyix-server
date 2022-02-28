422d787e4eb5a45596fc4756fc6472295e5f1aa3
KereBere <alsydm091@gmail.com>
2022-02-28 22:49:45 +0300

    Show last uploaded memes added

--- controllers/memeController.js
+++ controllers/memeController.js
@@ -22,7 +22,11 @@ export const getMemes = async (req, res) => {
   const startIndex = page * limit;
   let results;
   try {
-    results = await Meme.find().limit(limit).skip(startIndex).exec();
+    results = await Meme.find()
+      .sort("-createdAt")
+      .limit(limit)
+      .skip(startIndex)
+      .exec();
 
     res.send(results);
   } catch (error) {
