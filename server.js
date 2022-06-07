df1739771085d0d4f70a5ea1ca3fe88eeee8c9d9
KereBere <alsydm091@gmail.com>
2022-06-08 00:05:33 +0300

    Cors issue prevails somehow

--- server.js
+++ server.js
@@ -3,35 +3,29 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-dotenv.config(); 
-
+import routes from "./routes/index.js";
+dotenv.config();
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
+    const app = express();
+    app.use(cors());
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(cookieParser());
+    app.set("view engine", "ejs");
+
+    app.use("/", routes);
+
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`Server started with ${PORT} port`);
+    });
+
     console.log("Db connected");
   })
   .catch((err) => {
     console.log(err);
     console.log("Could not connect to DB");
   });
-
- 
-
-import routes from "./routes/index.js";
-dotenv.config();
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.set("view engine", "ejs");
- 
-
-
-app.use("/", routes);
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server started with ${PORT} port`);
-});
