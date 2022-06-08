1588be0c6c40ff581514232dd6f9aec7b5de9cb1
KereBere <alsydm091@gmail.com>
2022-06-08 12:04:28 +0300

    Cors persist :(

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
-import cors from "cors";
+const cors = require("cors");
 import routes from "./routes/index.js";
 dotenv.config();
 
